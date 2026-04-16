744cfb065160080013c79ffec5abce4366935a46
RhuanBoni <rhuanboni@newgesti.com.br>
2026-04-16 16:31:11 -0300

    feat: implement ParameterActivity UI using Jetpack Compose

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3043983/intentscompose/ParameterActivity.kt
@@ -0,0 +1,73 @@
+package br.edu.ifsp.scl.sc3043983.intentscompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class ParameterActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                var parameterText by remember { mutableStateOf("") }
+
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Parameter") },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                            )
+                        )
+                    },
+                    content = { innerPadding ->
+                        Column (
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize()
+                                .padding(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            OutlinedTextField(
+                                value = parameterText,
+                                onValueChange = { parameterText = it },
+                                label = { Text("Parameter") },
+                                modifier = Modifier.fillMaxWidth()
+                            )
+
+                            Button(
+                                onClick = {},
+                                modifier = Modifier.fillMaxWidth()
+                            ) {
+                                Text("Save and quit")
+                            }
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
