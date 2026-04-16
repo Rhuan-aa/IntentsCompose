ef42b664d461fbe74195214e95641d768d3d866b
RhuanBoni <rhuanboni@newgesti.com.br>
2026-04-16 17:09:16 -0300

    feat: implement MainActivity UI with TopAppBar actions and centered text

--- app/src/main/java/br/edu/ifsp/scl/sc3043983/intentscompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3043983/intentscompose/MainActivity.kt
@@ -3,45 +3,95 @@ package br.edu.ifsp.scl.sc3043983.intentscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Language
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import br.edu.ifsp.scl.sc3043983.intentscompose.ui.theme.IntentsComposeTheme
-
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        
         setContent {
-            IntentsComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                var currentParameter by remember { mutableStateOf("Not set yet!!!") }
+
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Intents Compose") },
+                            colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                            ),
+                            actions = {
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        Icons.Default.Edit,
+                                        contentDescription = "Set Parameter"
+                                    )
+                                }
+
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        Icons.Default.Language,
+                                        contentDescription = "Web"
+                                    )
+                                }
+
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        Icons.Default.Phone,
+                                        contentDescription = "Dial"
+                                    )
+                                }
+
+                                IconButton(onClick = {}) {
+                                    Icon(
+                                        Icons.Default.Call,
+                                        contentDescription = "Call"
+                                    )
+                                }
+                            }
+
+                        )
+                    },
+                    content = { innerPadding ->
+                        Box(
+                            modifier = Modifier
+                                .padding(innerPadding)
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = currentParameter,
+                                fontSize = 24.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IntentsComposeTheme {
-        Greeting("Android")
     }
 }
